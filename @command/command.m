73419291845e7a9f20c1b1d05941de8bce26d92f
1432bbll <1754482587@qq.com>
2025-03-11 15:42:12 +0800

    1

---
+++ @command/command.m
@@ -0,0 +1,19 @@
+classdef command
+   %  properties
+   %    p1
+   %    p2
+   %    p3
+   % end
+   % methods
+   %     function c = command(r, g, b)
+   %       c.p1 = r; c.p2 = g; c.p3 = b;
+   %    end
+   % end
+   enumeration
+        Query
+        QueryRep
+        QueryAdjust
+        ACK
+        NAK
+    end
+end
